8960c9928b190dc83613b432f2e6d289191dc435
pramodaya <pramodayajayalath@gmail.com>
2019-02-20 10:45:02 +0530

    Ceeid project updated
    changed the .htaccess file, sidebar and base url.

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Oxo_Company_School_Management_System_0/';
+$config['base_url'] = 'http://localhost/Ceeids/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -45,7 +45,7 @@
                 <span>Assign Schools to Users</span></a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url();?>AssignSchools/index/">
+            <a class="nav-link" href="<?php echo base_url();?>schoolContacts/index/">
                 <i class="fas fa-fw fa-university"></i>
                 <span>Manage Schools</span>
             </a>
